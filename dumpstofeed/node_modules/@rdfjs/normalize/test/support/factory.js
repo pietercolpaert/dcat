2d18f90e3c2347b114bcd405fe2c385a86727c4d
Pieter Colpaert <pieter@openknowledge.be>
2024-02-11 14:19:02 +0100

    Delete dumpstofeed directory

--- dumpstofeed/node_modules/@rdfjs/normalize/test/support/factory.js
+++
@@ -1,7 +0,0 @@
-import DataModelFactory from '@rdfjs/data-model/Factory.js'
-import DatasetFactory from '@rdfjs/dataset/Factory.js'
-import Environment from '@rdfjs/environment'
-
-const rdf = new Environment([DataModelFactory, DatasetFactory])
-
-export default rdf
